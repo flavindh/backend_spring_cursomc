2f6efb154152fdf4d7c3869cb8dca168b54285f5
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-07-02 15:09:33 -0300

    Projeto criado

---
+++ src/main/java/com/flavio/cursomc/BackendSpringCursomcApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringCursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringCursomcApplication.class, args);
+	}
+
+}
