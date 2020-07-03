8bfa40899a9211716b67597261da475fc28d1d44
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-07-03 13:03:54 -0300

    Criando operacao de instanciacao

--- src/main/java/com/flavio/cursomc/BackendSpringCursomcApplication.java
+++ src/main/java/com/flavio/cursomc/BackendSpringCursomcApplication.java
@@ -1,13 +1,32 @@
 package com.flavio.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.flavio.cursomc.domain.Categoria;
+import com.flavio.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class BackendSpringCursomcApplication {
+public class BackendSpringCursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSpringCursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
