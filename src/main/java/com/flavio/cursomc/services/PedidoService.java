879a3a432d3c13b0c0dfcc45c9a0cfaef34b1f88
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-07-06 14:50:21 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/flavio/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.flavio.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flavio.cursomc.domain.Pedido;
+import com.flavio.cursomc.repositories.PedidoRepository;
+import com.flavio.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = pedidoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
