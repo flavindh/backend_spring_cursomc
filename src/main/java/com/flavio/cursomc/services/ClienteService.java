cc36a9d7890d80560c5ff2c4f3e74a25bd860ff4
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-07-06 13:51:33 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/flavio/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.flavio.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flavio.cursomc.domain.Cliente;
+import com.flavio.cursomc.repositories.ClienteRepository;
+import com.flavio.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
