78a343ca278c2e6169912bc814ed643f535c022c
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-07-07 14:53:38 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/flavio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/flavio/cursomc/resources/CategoriaResource.java
@@ -22,9 +22,8 @@ public class CategoriaResource {
 	private CategoriaService categoriaService;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = categoriaService.find(id);
-
 		return ResponseEntity.ok().body(obj);
 	}
 
@@ -32,8 +31,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = categoriaService.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-
 		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = categoriaService.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/flavio/cursomc/services/CategoriaService.java
+++ src/main/java/com/flavio/cursomc/services/CategoriaService.java
@@ -17,14 +17,17 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
 
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return categoriaRepository.save(obj);
 	}
 
