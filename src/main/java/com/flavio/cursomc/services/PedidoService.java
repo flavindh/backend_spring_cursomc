bf4555e99fde37bdf06c5ce625d96510b2f79d70
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-09-15 09:16:57 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/flavio/cursomc/services/PedidoService.java
+++ src/main/java/com/flavio/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.flavio.cursomc.domain.Cliente;
 import com.flavio.cursomc.domain.ItemPedido;
 import com.flavio.cursomc.domain.PagamentoComBoleto;
 import com.flavio.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.flavio.cursomc.enums.EstadoPagamento;
 import com.flavio.cursomc.repositories.ItemPedidoRepository;
 import com.flavio.cursomc.repositories.PagamentoRepository;
 import com.flavio.cursomc.repositories.PedidoRepository;
+import com.flavio.cursomc.security.UserSS;
+import com.flavio.cursomc.services.exceptions.AuthorizationException;
 import com.flavio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -66,9 +72,18 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-//		emailService.sendOrderConfirmationEmail(obj);
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 
 }
