6838ac03801482e1c687918575f59b0dc33ed58c
Flavio Goncalves <flaviojgfilho@hotmail.com>
2020-10-01 16:19:15 -0300

    Endpoint para buscar cliente por email, estados e cidades

--- src/main/java/com/flavio/cursomc/services/ClienteService.java
+++ src/main/java/com/flavio/cursomc/services/ClienteService.java
@@ -106,4 +106,18 @@ public class ClienteService {
 		newObj.setEmail(obj.getEmail());
 	}
 
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Cliente obj = clienteRepository.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
 }
